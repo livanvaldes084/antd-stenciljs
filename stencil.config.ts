405db0fa35bafe947ff856ca98ee5c82a65053c4
Ing.Livan Alejandro Capote Valdés <lcapote@altimetrik.com>
2021-05-17 11:14:16 -0300

    Commit

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,18 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 export const config: Config = {
-  namespace: 'ant-stencil',
+  namespace: "storybook-stencil-example",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+      type: "dist",
+      esmLoaderPath: "../loader"
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: "docs-readme"
     },
     {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+      type: "www",
+      serviceWorker: null // disable service workers
+    }
   ],
 };
